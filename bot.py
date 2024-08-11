028d780f4600ce0d0e41751682c54b303e0d0b76
Hexa <shexa.developer@gmail.com>
2024-08-11 10:59:32 -0500

    Clean code

--- bot.py
+++ bot.py
@@ -23,22 +23,20 @@ def callback(client, callback_query: CallbackQuery):
 @app.on_message(filters.voice | filters.audio)
 def handle_audio(client, message):
     file = client.download_media(message.voice.file_id if message.chat.type == "voice" else message.audio.file_id)
-    audio = AudioSegment.from_file(file)
-    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_file:
-        audio.export(temp_file.name, format="mp3", bitrate="64k")
-        temp_file.seek(0)
+    audio = AudioSegment.from_file(file).set_channels(AUDIO_CHANNELS).set_frame_rate(AUDIO_SAMPLE_RATE)
+    with tempfile.NamedTemporaryFile(suffix=TEMP_FILE_SUFFIX_AUDIO, delete=False) as temp_file:
+        audio.export(temp_file.name, format=AUDIO_FORMAT, bitrate=AUDIO_BITRATE)
         message.reply_document(temp_file.name)
+    os.remove(file)
 
-@app.on_message(filters.video)
-def handle_video(client, message):
-    file = client.download_media(message.video.file_id)
-    
-    with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_file:
-        temp_filename = temp_file.name
-        command = f'ffmpeg -i "{file}" -filter_complex "scale=\'min(1920,iw)\':\'min(1080,ih)\'" -r 24 -c:v libx265 -pix_fmt yuv420p -b:v 100k -crf 30 -preset fast -c:a libfdk_aac -profile:a xhe-aac -sample_fmt s16 -ar 44100 -b:a 64k -ac 1 -map_metadata -1 "{temp_filename}"'
-        subprocess.run(command, shell=True, check=True)
-        message.reply_video(temp_filename)
-    
+@app.on_message(filters.video | filters.animation)
+def handle_media(client, message):
+    file = client.download_media(message.video.file_id if message.video else message.animation.file_id)
+    suffix = TEMP_FILE_SUFFIX_VIDEO if message.video else '.gif'
+    temp_filename = tempfile.NamedTemporaryFile(suffix=suffix, delete=False).name
+    command = f'ffmpeg -i "{file}" {"-vf scale={VIDEO_SCALE} -r {VIDEO_FPS}" if suffix == ".gif" else "-filter_complex scale={VIDEO_SCALE} -r {VIDEO_FPS} -c:v {VIDEO_CODEC} -pix_fmt {VIDEO_PIXEL_FORMAT} -b:v {VIDEO_BITRATE} -crf {VIDEO_CRF} -preset {VIDEO_PRESET} -c:a {VIDEO_AUDIO_CODEC} -b:a {VIDEO_AUDIO_BITRATE} -ac {VIDEO_AUDIO_CHANNELS} -ar {VIDEO_AUDIO_SAMPLE_RATE} -profile:v {VIDEO_PROFILE} -map_metadata -1"} "{temp_filename}"'
+    subprocess.run(command, shell=True, check=True)
+    (message.reply_document(temp_filename) if suffix == '.gif' else message.reply_video(temp_filename))
     os.remove(temp_filename)
 
 app.run()
